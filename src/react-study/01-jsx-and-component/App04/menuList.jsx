536e907a89587d0142124be1f87d3515969ae428
gunobo <startea0716@gmail.com>
2026-01-12 21:38:16 +0900

    commit and push 260112-03

--- src/react-study/01-jsx-and-component/App04/menuList.jsx
+++ src/react-study/01-jsx-and-component/App04/menuList.jsx
@@ -1,14 +1,14 @@
 // 컴포넌트는 함수다
 // 매개변수를 받을 수 있음
-// 부모 컴포넌트 자식컴포넌트로 넘긴느 매개변수 props라고 한다.
+// 부모 컴포넌트 자식컴포넌트로 넘기는 매개변수 props라고 한다.
 
 // 1. 부모 -> 자식만 전달 가능
-// 2. js객체({})로 데이터를 매개변수에 전달
-// {title: title, menus: menus, a: "아무거나"}
+// 2. js 객체({})로 데이터를 매개변수에 전달
+// {title: title, menus: menus}
 
 // 객체를 매개변수로 받는거라
 // 비구조 할당으로 받을 수 있음 - 권장
-export default function menuList({title, menus}) {
+export default function MenuList({title, menus}) {
   return (
     <div>
         <h2>{title}</h2>
