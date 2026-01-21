eb3bffa8dd58c1bd643c851529de1488db723ca0
gunobo <startea0716@gmail.com>
2026-01-21 12:28:04 +0900

    commit and push changes to main branch

--- src/react-study/study.jsx
+++ src/react-study/study.jsx
@@ -1,15 +1,19 @@
 import UseRef01 from "./04-useRef/UseRef01";
 import UseRef02 from "./04-useRef/UseRef02";
-import Emtion01 from "./Emotion/Emtion01";
-import Emtion02 from "./Emotion/Emtion02";
+import Emotion01 from "./Emotion/Emotion01";
+import Emotion02 from "./Emotion/Emotion02";
+import Router01 from "./ReactRouter/Router01/Router01";
+import Router02 from "./ReactRouter/Router02/Router02";
 
 export default function Study() {
     const stateStudy = {
         1: <UseRef01 />,
         2: <UseRef02 />,
-        3: <Emtion01 />,
-        4: <Emtion02 />
+        3: <Emotion01 />,
+        4: <Emotion02 />,
+        5: <Router01 />,
+        6: <Router02 />,
     };
 
-    return stateStudy[4];
+    return stateStudy[6];
 }
