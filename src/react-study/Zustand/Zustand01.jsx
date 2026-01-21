e5acdad8b017893b005deda0f412a12033b1f1a2
gunobo <startea0716@gmail.com>
2026-01-21 21:47:11 +0900

    commit and push 260121-02

---
+++ src/react-study/Zustand/Zustand01.jsx
@@ -0,0 +1,11 @@
+// npm install zustand
+// 원리 -> 자바스크립트(클로저)
+// 전역상태관리 라이브러리
+// Redux(어려움), Zustand(쉬움), Recoil(사라짐)
+// -> useState와는 분리된 상태저장소
+
+export default function Zustand01() {
+  return (
+    <div>Zustand01</div>
+  )
+}
